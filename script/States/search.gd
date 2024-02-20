583409c6df69d53eb6d04b8c42040b5535bacacb
Andrew Bruce <andrew@andrewbruce.me>
2024-02-20 22:13:25 +1000

    improved animations

--- script/States/search.gd
+++ script/States/search.gd
@@ -18,13 +18,13 @@ var lastMoveDir : Vector3
 var turnBlend := -1.0
 var turnCoolDown := 0.0
 var lastSpeed = moveSpeed
+var lastRotation : float
 
 @export var ap  : AnimationPlayer
 
 func Enter():
-	#ap.play("walk")
+	ap.play("walking")
 	human.velocity = Vector3(1.99, 0, 0)
-	at["parameters/walk_stop_blend/blend_amount"] = turnBlend
 	
 func Update(delta : float):
 	if turnCoolDown > 0:
@@ -36,69 +36,57 @@ func Update(delta : float):
 		Wander(delta)
 
 
-
 func PhysicsUpdate(delta : float):
 	human.velocity = human.velocity.lerp(moveDirection * moveSpeed, delta)
-	human.rotation.y = lerp(human.rotation.y, -moveDirection.x*1.55, delta*2)
 
-	if stopping && turnBlend < 0:
-		turnBlend += delta
-		at["parameters/walk_stop_blend/blend_amount"] = turnBlend
-	elif stopping && turnBlend >= 0:
-		stopping = false
-		TurnAround()
-	elif turning && turnBlend <= 1:
-		turnBlend += delta
-		at["parameters/walk_stop_blend/blend_amount"] = turnBlend
-	elif turning && turnBlend >= 1:
-		Walk()
-	elif !turning && !stopping && turnBlend > -1:
-		moveSpeed = abs(turnBlend * lastSpeed)
-		turnBlend -= delta
-		at["parameters/walk_stop_blend/blend_amount"] = turnBlend
-	# elif turnBlend == -1:
-	# 	ap.speed_scale = moveSpeed / 2
+	if turning:
+		human.rotation.y = lerp(human.rotation.y, -moveDirection.x*1.55, delta*2)
+
+		if moveDirection.x == -1:
+			ap.play("left-turn-in-place")
+		elif moveDirection.x == 1:
+			ap.play("right-turn-in-place")
+
+		if (lastRotation < 0 && human.rotation.y >= 1.5) || (lastRotation > 0 && human.rotation.y <= -1.5):
+			turning = false
+			print_debug("last rotation: %s" % lastRotation)
+			print_debug("current rotation: %s" % human.rotation.y)
+
+			ap.play("walking")
+			human.velocity = Vector3(1.99*moveDirection.x, 0, 0)
+			moveSpeed = 2
 		
-
 
 func Wander(delta : float):
 	if wanderTime > 0:
 		wanderTime -= delta
-	else:
-		StopWalking()
+	# else:
+	# 	StopWalking()
+
 
 func _on_sight_component_near_wall():
-	StopWalking()
+		StopWalking()
+
 
 func StopWalking():
-	moveSpeed = 0.001
-	stopping = true
-	#ap.play("stop-walking")
+	if turnCoolDown <= 0:
+		ap.play("stop-walking", 1)
+		moveSpeed = 0
+		turnCoolDown = 5
+
 
 func TurnAround():
-	if turnCoolDown <= 0:
-		human.velocity = Vector3.ZERO
-		turning = true
-		#ap.play("left-turn-still")
-		turnCoolDown = 5
-		moveDirection.x *= -1
-		turnTimer = 1
-		wanderTime = randf_range(10, 20)
+	turning = true
+	moveDirection.x *= -1
+	wanderTime = randf_range(10, 20)
+	lastRotation = human.rotation.y
 
-func Walk():
-	#ap.play("walk")
-	moveSpeed = 2
-	#human.velocity = Vector3(1.99*moveDirection.x, 0, 0)
-	turning = false
 
 func _on_fear_component_scared():
 	Transitioned.emit(self, "scared")
 
 
 func _on_animation_player_animation_finished(anim_name):
-		if anim_name == "stop-walking":
-			human.velocity = Vector3.ZERO
-			await get_tree().create_timer(0.5).timeout
-			TurnAround()
-		elif anim_name == "left-turn-still":
-			Walk()
+	if anim_name == "stop-walking":
+		human.velocity = Vector3.ZERO
+		TurnAround()
